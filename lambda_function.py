b5360c5d223dad2166f2c1dc3654435d269be2c1
Deepak Sharma <ds9199027@gmail.com>
2024-11-12 00:12:39 +0530

    final code deployed..

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,10 @@ import json
 
 def deepak(event, context):
     # TODO implement
-    print("Hello I am Deepak")
-    return {
+ 
+ print("Hello I am Deepak")
+ 
+ return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda !! ')
+        'body': json.dumps('Hello from deepak & the code build successfully to lambda function !! ')
     }
